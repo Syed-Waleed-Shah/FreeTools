c58c7c2b70268613dba8b827d1895ce701ac3f90
Syed-Waleed-Shah <55924255+Syed-Waleed-Shah@users.noreply.github.com>
2021-05-18 09:33:28 -0700

    Several changes see description
    app.py : Handled exceptions, added functions and route for playlist downnloader
    
    base.html : Updated some jinja rendering code

--- app.py
+++ app.py
@@ -2,37 +2,132 @@ from flask import Flask, render_template, request, send_from_directory, jsonify,
 from flask_restful import Resource, Api, reqparse, abort 
 import _thread as thread
 import subprocess
-from pytube import YouTube
+from pytube import YouTube, Playlist
+from pytube.exceptions import VideoUnavailable
+from sanitize_filename import sanitize
 import time
+import random
 import os
 
 
-def downloadVideo(url):
-    yt = YouTube(url)
-    yt.streams.filter(progressive=True, file_extension="mp4").order_by('resolution').desc().first().download(app.root_path + "\\Videos",max_retries=3)
+def downloadVideo(url, itag, title):
+    while True:
+        try:
+            yt = YouTube(url)
+            yt.streams.get_by_itag(itag).download(app.root_path + "\\Videos", sanitize(title),   max_retries=5)
+        except:
+            pass
+        else:
+            break
     
 
+def getVideDetails(url):   
+    try:
+        yt = YouTube(url)
+    except:
+        return {"error":404}
+    else:
+        while True:
+            try:
+                videos = yt.streams.filter(file_extension="mp4").filter(only_video=True)
+            except:
+                pass
+            else:
+                break
+
+        videosjson = []
+        for video in videos:
+            videosjson.append(
+                {
+                    "itag":video.itag, "mime_type":video.mime_type, "resolution":video.resolution
+                }
+            )       
+
+        return {
+            "title":yt.title, "thumbnail":yt.thumbnail_url, "views":yt.views, "url":url,
+            "videos":videosjson
+        }
+
+def getPlaylistDetails(url):
+    result = []
+    try:
+        yt = Playlist(url)  
+    except VideoUnavailable:
+        pass
+    else:
+        videos = yt.videos
+        for video in videos:       
+            while True:            
+                try: 
+                    video_streams = video.streams.filter(file_extension="mp4").filter(only_video=True)
+                except:
+                    pass
+                else:
+                    break
+
+
+            videosjson = []
+            for v in video_streams:
+                videosjson.append(
+                    {
+                        "itag":v.itag, "mime_type":v.mime_type, "resolution":v.resolution
+                    }
+                )           
+            result.append({
+            "title":video.title, "thumbnail":video.thumbnail_url, "views":video.views, "url":"https://youtube.com/watch?v="+video.video_id, "id":video.video_id,
+            "videos":videosjson
+            })
+
+    return result
 
 
 app = Flask(__name__)
 api = Api(app)
 @app.route('/', methods=["GET", "POST"])
-def index():
+def index():    
     if request.method == "POST" and request.form.get("YoutubeVideoDownload"):
-        # url = request.form.get("url")
-        # from pytube import YouTube
-        # path = YouTube(url).streams[0].download()
-        return send_from_directory(os.path.join(app.root_path, 'static'), "style.css")
+        url = request.form.get("url")
+        videodetails = getVideDetails(url)       
+        return render_template('youtube.html', videodetails=videodetails)
     return render_template('youtube.html')
+
+
+# Youtube video downloader
+@app.route('/tools/youtube/youtube-video-downloader', methods=["GET", "POST"])
+def youtubevideodownloader():
+    if request.method == "POST" and request.form.get("YoutubeVideoDownload"):
+        url = request.form.get("url")
+        videodetails = getVideDetails(url)       
+        return render_template('youtube-video.html', videodetails=videodetails)
+    return render_template('youtube-video.html')
+
+# Youtube playlist downloader
+@app.route('/tools/youtube/youtube-playlist-downloader', methods=["GET", "POST"])
+def youtubeplaylistdownloader():
+    if request.method == "POST" and request.form.get("YoutubeVideoDownload"):
+        url = request.form.get("url")
+        playlistdetails = getPlaylistDetails(url)       
+        return render_template('youtube-playlist.html', playlistdetails=playlistdetails)
+    return render_template('youtube-playlist.html')
 
 
 @app.route('/download')
 def download():
     url = request.args.get("url")    
+    itag = request.args.get("itag")
     video = YouTube(url)
-    thread.start_new_thread(downloadVideo, (url, ))
-    time.sleep(5)
-    return send_file(app.root_path + "\\Videos\\" + video.title+".mp4", as_attachment=True)
+    title = "video_" + str(random.randrange(1000,20000))
+    thread.start_new_thread(downloadVideo, (url, itag, title, ))    
+    # return send_file(app.root_path + "\\Videos\\" + sanitize(title)+".mp4", as_attachment=True)
+    while True:
+        try:
+            return send_file(app.root_path + "\\Videos\\" + sanitize(title)+".mp4", as_attachment=True)
+        except Exception as e:
+            print(e)
+        else:
+            break
+
+
 
 @app.route('/test')
 def test():
@@ -48,18 +143,29 @@ class YoutubeVideoInfo(Resource):
     def get(self):
         from pytube import YouTube
         url = request.args.get("url")
-        video = YouTube(url)
-        return jsonify({
-            "meta": {
-                "status":"200"
-            },
-            "data": {
-            "title":video.title,
-            "thumbnailUrl":video.thumbnail_url,
-            "views":video.views,
-            "id":video.video_id,                    
-            }
-        })
+        yt = YouTube(url)
+        audios =yt.streams.filter(only_audio=True)
+        videos = yt.streams.filter(only_video=True)
+        videosjson = []
+        audiosjson = []
+        for video in videos:
+            videosjson.append(
+                {
+                    "itag":video.itag, "mime_type":video.mime_type, "resolution":video.resolution
+                }
+            )
+        for audio in audios:
+            audiosjson.append(
+                {
+                    "itag":audio.itag, "mime_type":audio.mime_type, "resolution":audio.abr
+                }
+            )
+
+        return {
+            "title":yt.title, "thumbnail":yt.thumbnail_url, "views":yt.views,
+            "videos":videosjson, "audios":audiosjson
+        }
+
 
 # ----------------------------------------------------
 # ALL ROUTES OF API
--- test.py
+++ test.py
@@ -1,10 +1,58 @@
 import subprocess
 import os
-from pytube import YouTube
+from pytube import YouTube, Playlist
 url = "https://www.youtube.com/watch?v=sy1MNWt7om4"
-video = YouTube(url)
-subprocess.Popen("pytube {0}".format(url))
-# while True:
-#     if os.path.exists(video.title):
-#         break
-print("Download Started:",video.title)
+
+
+def getVideoInfo(url):
+    video = YouTube(url)
+    audios = video.streams.filter(only_audio=True)
+    videos = video.streams.filter(only_video=True)
+    for audio in audios:
+        print(audio.title)
+        print(audio.mime_type)
+        print(audio.abr)
+    # print(audio.filesize)
+    # print("%.2f" % round(audio.filesize/1024/1024,2))
+
+    for video in videos:
+        print(video.mime_type)
+        print(video.resolution)
+
+
+def getPlaylistInfo(url):
+    result = []
+    yt = Playlist(url)
+    videos = yt.videos
+    for video in videos:
+        audio_streams = video.streams.filter(only_audio=True)
+        video_streams = video.streams.filter(only_video=True)
+        videosjson = []
+        audiosjson = []
+        for v in video_streams:
+            videosjson.append(
+                {
+                    "itag":v.itag, "mime_type":v.mime_type, "resolution":v.resolution
+                }
+            )
+        for a in audio_streams:
+            audiosjson.append(
+                {
+                    "itag":a.itag, "mime_type":a.mime_type, "resolution":a.abr
+                }
+            )
+
+        result.append({
+        "title":video.title, "thumbnail":video.thumbnail_url, "views":video.views, "url":"https://youtube.com/watch?v="+video.video_id,
+        "videos":videosjson, "audios":audiosjson
+        })
+
+    return result
+
+
+if __name__ == "__main__":
+    playlist = getPlaylistInfo("https://www.youtube.com/playlist?list=PLXkEfqYbulLEUsu_kvIcbi8JeKN2iMoux")
+    print(playlist)
+    
+
+    
